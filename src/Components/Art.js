90b8c8b9e7e4230c1677010913bd95f82827894d
Brandon Garcia <grandonleebarcia@gmail.com>
2020-11-28 13:03:29 -0800

    first attempt to add aesthic to background of page but still working out design

---
+++ src/Components/Art.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Row, Col } from 'react-bootstrap/'
+const Art = () => {
+    const style = {
+        position: 'absolute',
+        height: '100vh'
+    }
+    return (
+        <Row style={style}>
+            <Col>
+                let
+                const
+                var
+            </Col>
+            <Col>
+                let
+                const
+                var
+            </Col>
+        </Row>
+    )
+}
+
+export default Art
