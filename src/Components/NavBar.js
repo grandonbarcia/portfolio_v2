e05a5af5e605892474d1683c60b32ff81b528864
Brandon Garcia <grandonleebarcia@gmail.com>
2020-11-28 13:02:23 -0800

    added smooth scroll links to nav bar

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,18 +1,19 @@
 import React from 'react'
 import { Container, Nav, Navbar } from 'react-bootstrap'
+import AnchorLink from 'react-anchor-link-smooth-scroll'
 
 const Navi = () => {
     return (
-        <Navbar bg="light" expand="lg">
+        <Navbar bg="light" expand="lg" fixed='top'>
             <Container fluid={'xl'} >
                 <Navbar.Brand>Brandon Garcia</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse>
                     <Nav className="ml-auto">
-                        <Nav.Link >Home</Nav.Link>
-                        <Nav.Link >About Me</Nav.Link>
-                        <Nav.Link >Projects</Nav.Link>
-                        <Nav.Link >Contact</Nav.Link>
+                        <Nav.Link as={AnchorLink} offset='100' href="#home" ><AnchorLink>Home</AnchorLink></Nav.Link>
+                        <Nav.Link as={AnchorLink} offset='100' href="#about" >About Me</Nav.Link>
+                        <Nav.Link as={AnchorLink} offset='100' href="#projects" >Projects</Nav.Link>
+                        <Nav.Link as={AnchorLink} offset='100' href="#contact" >Contact</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
