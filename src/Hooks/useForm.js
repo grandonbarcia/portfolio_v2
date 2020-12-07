d0cc5220d70b7a9040d54f3cfee073141beb0989
Brandon Garcia <grandonleebarcia@gmail.com>
2020-12-07 13:59:50 -0800

    added useForm hook to get values from form

---
+++ src/Hooks/useForm.js
@@ -0,0 +1,46 @@
+import { useState } from 'react';
+
+const useForm = () => {
+
+    let [messageInput, setMessageInput] = useState({
+        name: '',
+        subject: '',
+        company: '',
+        email: '',
+        message: ''
+    })
+
+
+    let [isFilled, isFormFilled] = useState(true);
+
+    const handleChange = e => {
+        console.log(e.target.value);
+        const { name, value } = e.target;
+        setMessageInput({
+            ...messageInput,
+            [name]: value,
+        });
+        isFormFilled(checkFormPopulation(value))
+    }
+
+    const clearForm = () => {
+        setMessageInput({});
+    }
+
+    const checkFormPopulation = (form) => {
+        let count = 0;
+        let arr = Object.entries(form);
+        for (let [key, value] of arr) {
+            if (value.length > 4) {
+                count++;
+            }
+        }
+        if (count == arr.length) {
+            return false
+        }
+        return true
+    }
+    return { handleChange, clearForm, messageInput, isFilled };
+}
+
+export default useForm; 
