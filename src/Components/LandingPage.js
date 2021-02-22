05e01afab8db7dacc47fdb47445850ebb28ed585
Brandon Garcia <grandonleebarcia@gmail.com>
2021-02-22 14:50:35 -0800

    left aligned text, increased font

--- src/Components/LandingPage.js
+++ src/Components/LandingPage.js
@@ -1,67 +1,39 @@
 import React from 'react'
-import { Row, Col } from 'react-bootstrap/'
+import { Row, Col, Button } from 'react-bootstrap/'
 import { Browser } from 'react-kawaii'
 import '../App.css';
 
 const LandingPage = () => {
     return (
-        <section id="about" >
-            <Row>
-                <Col className='text-center main___title'>
-                    <h1>Hi, I'm Brandon</h1>
-                    <h1>Software Developer </h1>
-                    <h3 style={{ color: 'gray' }}>I like solving problems one app at a time. </h3>
-                </Col>
-            </Row>
-            <Row className='title'>
-                <Col className='text-center' sm={{ span: 4, offset: 4 }} md={{ span: 3, offset: 2 }} lg={{ span: 2, offset: 2 }} xl={{ span: 2, offset: 2 }} >
-                    <h3>About Me</h3>
-                </Col>
-            </Row>
-            <Row className='justify-content-md-center'>
-                <Col className='text-center' md={4} lg={3} xl={3}>
-                    <Row>
+        <section id="home" >
+            <Row className='justify-content-md-center' >
+                <Col md={10} lg={10} xl={12} className='main__title'>
+                    <Row className="main__text">
                         <Col>
-                            <Browser size={150} mood="happy" color="#61DDBC" />
+                            Hi, I'm Brandon
                         </Col>
                     </Row>
-                </Col>
-                <Col xs={{ span: 10, offset: 1 }} sm={{ span: 9, offset: 2 }} md={{ span: 5, offset: 0 }} lg={{ span: 6, offset: 0 }} xl={{ span: 6, offset: 0 }}>
-                    <Row className="sub--text">
+                    <Row className="main__text">
                         <Col>
-                            Creating web apps has always fascinated me, that's why I decided to turn this interest into a career. Despite acquiring considerable knowledge, I do not take it for granted. I constantly try to develop, which allows me to level up my code and create more dynamic projects.
-                         </Col>
+                            Front-End Developer
+                        </Col>
                     </Row>
-                    <Row className="sub--text">
+                    <Row>
                         <Col>
-                            Here are some technologies I’ve been working with recently:
-                        <Row>
-                                <Col>
-                                    Front-end:
-                                <ul>
-                                        <li>
-                                            React
-                                    </li>
-                                        <li>
-                                            Bootstrap
-                                    </li>
-                                    </ul>
-                                </Col>
-                                <Col>
-                                    Back-end
-                                <ul>
-                                        <li>
-                                            Node.js
-                                    </li>
-                                        <li>
-                                            Express.js
-                                    </li>
-                                        <li>
-                                            MongoDB
-                                    </li>
-                                    </ul>
-                                </Col>
-                            </Row>
+                            <h3 style={{ color: 'gray' }}>I like solving problems one app at a time. </h3>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col className="sub__text" xl={8}>
+                            <p>Ambitious software developer specializing in MERN technologies. Passionate about technology, learning new things, and growing as a developer. </p>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col xl={2}>
+                            <Button variant={'outline-primary'}>Let's Chat</Button>
+                        </Col>
+                        <Col xl={2}>
+                            <Button variant={'outline-danger'}>Resume</Button>
                         </Col>
                     </Row>
                 </Col>
