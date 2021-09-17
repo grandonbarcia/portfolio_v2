bdf638fd2f7f95eb3cfef620c1f4dc390f19896c
Brandon Garcia <grandonleebarcia@gmail.com>
2021-09-16 18:29:58 -0700

    align odd numbered project title and description text to right

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,27 +1,25 @@
-import React from 'react'
-import { Row, Col, Image } from 'react-bootstrap/'
-import HabitTracker from '../img/habit-tracker.JPG'
-import BrainGames from '../img/brain-games.jpg'
+import React from "react";
+import { Row, Col, Image } from "react-bootstrap/";
+import HabitTracker from "../img/habit-tracker.JPG";
+import BrainGames from "../img/brain-games.jpg";
 // import CovidTracker from '../img/covid19.JPG'
-import TypingTest from '../img/typing-test.JPG'
-import { HiOutlineDesktopComputer } from 'react-icons/hi'
-import { AiOutlineGithub } from 'react-icons/ai'
-import '../App.css';
-
-
+import TypingTest from "../img/typing-test.JPG";
+import { HiOutlineDesktopComputer } from "react-icons/hi";
+import { AiOutlineGithub } from "react-icons/ai";
+import "../App.css";
 
 const Projects = () => {
-    return (
-        <section id='projects'>
-            <Row className="justify-content-center">
-                <Col lg={10} xl={12}>
-                    <Row className='title'>
-                        <Col className="text-center">
-                            <h2>Personal Projects</h2>
-                        </Col>
-                    </Row>
+  return (
+    <section id="projects">
+      <Row className="justify-content-center">
+        <Col lg={10} xl={12}>
+          <Row className="title">
+            <Col className="text-center">
+              <h2>Personal Projects</h2>
+            </Col>
+          </Row>
 
-                    {/* <Row className='project__row'>
+          {/* <Row className='project__row'>
                         <Col className='project__image__col' lg={6} xl={6}>
                             <Image className="project__image shadow-lg " src={CovidTracker} />
                         </Col >
@@ -45,73 +43,149 @@ const Projects = () => {
                         </Col>
                     </Row> */}
 
-                    <Row className='project__row'>
-                        <Col lg={6} xl={5} className="my-auto">
-                            <Row>
-                                <Col>
-                                    <h4 className="title__text">Brain Games </h4>
-                                    <p className="sub__text">I had the idea to develop types of mini game that can test the users basic gaming skills. Inspired by HumanBenchmark, I built a reaction time game and a target practice game. Users are prompt with the rules and a start condition. After the game is finish, the user recieves their respective averages for each game. </p>
-                                    <Row className="justify-content-end">
-                                        <Col xs={2} sm={2} md={1} lg={2} xl={2}>
-                                            <a href="https://github.com/grandonbarcia/mini-games" target='_blank' rel='noreferrer'><AiOutlineGithub size={32} /></a>
-                                        </Col>
-                                        <Col xs={2} sm={2} md={1} lg={2} xl={2} >
-                                            <a href="https://focused-bassi-d58606.netlify.app/" target='_blank' rel='noreferrer'><HiOutlineDesktopComputer size={32} /> </a>
-                                        </Col>
-                                    </Row>
-                                </Col>
-                            </Row>
-                        </Col>
-                        <Col className='project__image__col' lg={6} xl={{ span: 6, offset: 1 }}>
-                            <Image className="project__image shadow-lg " src={BrainGames} />
-                        </Col >
-                    </Row>
-
-                    <Row className='project__row'>
-                        <Col className='project__image__col' lg={6} xl={6} >
-                            <Image className="project__image shadow-lg" src={HabitTracker} width={'100%'} height={'100%'} />
-                        </Col>
-                        <Col lg={6} xl={{ span: 5, offset: 1 }} className=" my-auto">
-                            <h4 className="title__text">Habit Tracker </h4>
-                            <p className="sub__text">I wanted to improve my lifestyle a bit by adding some habits that may be beneficial, like meditating and stretching. After a while, I had a hard time keeping track of how many times I did each of these tasks per week. That's why I created a tracker to help me visualize my habits for the month.</p>
-                            <Row className="justify-content-end">
-                                <Col xs={2} sm={2} md={1} lg={2} xl={2}>
-                                    <a href='https://github.com/grandonbarcia/habbit-tracker' target='_blank' rel="noreferrer"><AiOutlineGithub size={32} /></a>
-                                </Col>
-                                <Col xs={2} sm={2} md={1} lg={2} xl={2} >
-                                    <a href='https://epic-bell-c4bb6e.netlify.app/' target='_blank' rel='noreferrer'><HiOutlineDesktopComputer size={32} /></a>
-                                </Col>
-                            </Row>
-                        </Col>
-                    </Row>
-
-                    <Row className='project__row'>
-                        <Col lg={6} xl={5} className="my-auto">
-                            <Row>
-                                <Col>
-                                    <h4 className="title__text">Typing Test</h4>
-                                    <p className="sub__text">After receiving my new keyboard, I coudn't resist the urge to keep typing on it. That's why I made this typing test. After users hit the start button, they are prompted with a quote and text field. When they are finished, their WPM (words per minute) will be displayed. I challenge you to beat your best! Working on implementing a graph to display user's past scores. </p>
-                                    <Row className="justify-content-end">
-                                        <Col xs={2} sm={2} md={1} lg={2} xl={2}>
-                                            <a href="https://github.com/grandonbarcia/typing-test" target='_blank' rel="noreferrer"><AiOutlineGithub size={32} /></a>
-                                        </Col>
-                                        <Col xs={2} sm={2} md={1} lg={2} xl={2} >
-                                            <a href="https://vigilant-spence-6f0b27.netlify.app/" target='_blank' rel='noreferrer'><HiOutlineDesktopComputer size={32} /> </a>
-                                        </Col>
-                                    </Row>
-                                </Col>
-                            </Row>
-                        </Col>
-                        <Col className='project__image__col' lg={6} xl={{ span: 6, offset: 1 }}>
-                            <Image className="project__image shadow-lg " src={TypingTest} />
-                        </Col >
-                    </Row>
-
-
+          <Row className="project__row">
+            <Col lg={6} xl={5} className="my-auto">
+              <Row>
+                <Col>
+                  <Row>
+                    <Col>
+                      <h4 className="title__text text-right">Brain Games </h4>
+                    </Col>
+                  </Row>
+                  <Row>
+                    <Col>
+                      <p className="sub__text text-right">
+                        I had the idea to develop types of mini game that can
+                        test the users basic gaming skills. Inspired by
+                        HumanBenchmark, I built a reaction time game and a
+                        target practice game. Users are prompt with the rules
+                        and a start condition. After the game is finish, the
+                        user recieves their respective averages for each game.{" "}
+                      </p>
+                    </Col>
+                  </Row>
+                  <Row className="justify-content-end">
+                    <Col xs={2} sm={2} md={1} lg={2} xl={2}>
+                      <a
+                        href="https://github.com/grandonbarcia/mini-games"
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        <AiOutlineGithub size={32} />
+                      </a>
+                    </Col>
+                    <Col xs={2} sm={2} md={1} lg={2} xl={2}>
+                      <a
+                        href="https://focused-bassi-d58606.netlify.app/"
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        <HiOutlineDesktopComputer size={32} />{" "}
+                      </a>
+                    </Col>
+                  </Row>
                 </Col>
-            </Row>
-        </section>
-    )
-}
+              </Row>
+            </Col>
+            <Col
+              className="project__image__col"
+              lg={6}
+              xl={{ span: 6, offset: 1 }}
+            >
+              <Image className="project__image shadow-lg " src={BrainGames} />
+            </Col>
+          </Row>
 
-export default Projects
+          <Row className="project__row">
+            <Col className="project__image__col" lg={6} xl={6}>
+              <Image
+                className="project__image shadow-lg"
+                src={HabitTracker}
+                width={"100%"}
+                height={"100%"}
+              />
+            </Col>
+            <Col lg={6} xl={{ span: 5, offset: 1 }} className=" my-auto">
+              <h4 className="title__text">Habit Tracker </h4>
+              <p className="sub__text">
+                I wanted to improve my lifestyle a bit by adding some habits
+                that may be beneficial, like meditating and stretching. After a
+                while, I had a hard time keeping track of how many times I did
+                each of these tasks per week. That's why I created a tracker to
+                help me visualize my habits for the month.
+              </p>
+              <Row className="justify-content-end">
+                <Col xs={2} sm={2} md={1} lg={2} xl={2}>
+                  <a
+                    href="https://github.com/grandonbarcia/habbit-tracker"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <AiOutlineGithub size={32} />
+                  </a>
+                </Col>
+                <Col xs={2} sm={2} md={1} lg={2} xl={2}>
+                  <a
+                    href="https://epic-bell-c4bb6e.netlify.app/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <HiOutlineDesktopComputer size={32} />
+                  </a>
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+
+          <Row className="project__row">
+            <Col lg={6} xl={5} className="my-auto">
+              <Row>
+                <Col>
+                  <h4 className="title__text text-right">Typing Test</h4>
+                  <p className="sub__text text-right">
+                    After receiving my new keyboard, I coudn't resist the urge
+                    to keep typing on it. That's why I made this typing test.
+                    After users hit the start button, they are prompted with a
+                    quote and text field. When they are finished, their WPM
+                    (words per minute) will be displayed. I challenge you to
+                    beat your best! Working on implementing a graph to display
+                    user's past scores.{" "}
+                  </p>
+                  <Row className="justify-content-end">
+                    <Col xs={2} sm={2} md={1} lg={2} xl={2}>
+                      <a
+                        href="https://github.com/grandonbarcia/typing-test"
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        <AiOutlineGithub size={32} />
+                      </a>
+                    </Col>
+                    <Col xs={2} sm={2} md={1} lg={2} xl={2}>
+                      <a
+                        href="https://vigilant-spence-6f0b27.netlify.app/"
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        <HiOutlineDesktopComputer size={32} />{" "}
+                      </a>
+                    </Col>
+                  </Row>
+                </Col>
+              </Row>
+            </Col>
+            <Col
+              className="project__image__col"
+              lg={6}
+              xl={{ span: 6, offset: 1 }}
+            >
+              <Image className="project__image shadow-lg " src={TypingTest} />
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+    </section>
+  );
+};
+
+export default Projects;
