03bf2022762a0e8dbdc17ac52fa31b60722c5a6d
Brandon Garcia <grandonleebarcia@gmail.com>
2021-10-11 21:30:45 -0700

    intial redesign commit

--- src/Components/LandingPage.js
+++ src/Components/LandingPage.js
@@ -1,47 +1,46 @@
-import React from 'react'
-import { Row, Col, Button } from 'react-bootstrap/'
-import { Browser } from 'react-kawaii'
-import Resume from '../img/Brandon_Garcia_Resume.pdf'
-import AnchorLink from 'react-anchor-link-smooth-scroll'
-import '../App.css';
+import React from "react";
+import { Row, Col, Button } from "react-bootstrap/";
+import { Browser } from "react-kawaii";
+import Resume from "../img/Brandon_Garcia_Resume.pdf";
+import AnchorLink from "react-anchor-link-smooth-scroll";
+import "../App.css";
 
 const LandingPage = () => {
-    return (
-        <section id="home" >
-            <Row className='justify-content-md-center' >
-                <Col xs={12} md={10} lg={10} xl={12} className='main__title'>
-                    <Row className="main__text">
-                        <Col>
-                            Hi, I'm Brandon
-                        </Col>
-                    </Row>
-                    <Row className="main__text">
-                        <Col>
-                            Front-End Developer
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col>
-                            <h3 style={{ color: 'gray' }}>I like solving problems one app at a time. </h3>
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col className="sub__text" xl={8}>
-                            <p>Ambitious software developer specializing in MERN technologies. Passionate about technology, learning new things, and growing as a developer. </p>
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col xs={5} sm={2} md={3} lg={2} xl={2}>
-                            <Button as={AnchorLink} variant={'outline-primary'} href="#contact">Let's Chat</Button>
-                        </Col>
-                        <Col xs={3} sm={2} md={3} lg={2} xl={2}>
-                            <a href={Resume} target='_blank' rel='noopener noreferrer'><Button variant={'outline-danger'}>Resume</Button></a>
-                        </Col>
-                    </Row>
-                </Col>
-            </Row>
-        </section >
-    )
-}
+  return (
+    <section id="home">
+      <div className="square1"></div>
+      <div className="square2"></div>
+      <Row className="justify-content-center align-items-center h-50">
+        <Col xl={6}>
+          <Row>
+            <Col>
+              <div className="title">Front End Developer</div>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <div className="sub__title">
+                I like solving problems one app at a time
+              </div>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+      <div class="custom-shape-divider-bottom-1634008616">
+        <svg
+          data-name="Layer 1"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 1200 120"
+          preserveAspectRatio="none"
+        >
+          <path
+            d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
+            class="shape-fill"
+          ></path>
+        </svg>
+      </div>
+    </section>
+  );
+};
 
-export default LandingPage
+export default LandingPage;
