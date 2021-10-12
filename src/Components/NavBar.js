2e1c2e37c808b2662c338212778266a34c4dd74b
Brandon Garcia <grandonleebarcia@gmail.com>
2021-10-11 21:31:15 -0700

    removed props from Navbar changed font color to green

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,23 +1,44 @@
-import React from 'react'
-import { Container, Nav, Navbar } from 'react-bootstrap'
-import AnchorLink from 'react-anchor-link-smooth-scroll'
+import React from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import AnchorLink from "react-anchor-link-smooth-scroll";
 
 const Navi = () => {
-    return (
-        <Navbar className="navBar" variant="dark" bg="dark" expand="sm" fixed='top'>
-            <Container fluid={'md'}>
-                <Navbar.Brand className="name">Brandon Garcia</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse>
-                    <Nav className="ml-auto">
-                        <Nav.Link as={AnchorLink} offset='100' className="link" href="#about" >About Me</Nav.Link>
-                        <Nav.Link as={AnchorLink} offset='150' className="link" href="#projects" >Projects</Nav.Link>
-                        <Nav.Link as={AnchorLink} offset='100' className="link" href="#contact" >Contact</Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
-    )
-}
+  return (
+    <Navbar fixed="top">
+      <Container fluid={"md"}>
+        <Navbar.Brand className="name">Brandon Garcia</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse>
+          <Nav className="ml-auto">
+            <Nav.Link
+              as={AnchorLink}
+              offset="100"
+              className="link"
+              href="#about"
+            >
+              About Me
+            </Nav.Link>
+            <Nav.Link
+              as={AnchorLink}
+              offset="150"
+              className="link"
+              href="#projects"
+            >
+              Projects
+            </Nav.Link>
+            <Nav.Link
+              as={AnchorLink}
+              offset="100"
+              className="link"
+              href="#contact"
+            >
+              Contact
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
 
-export default Navi
+export default Navi;
