05a65f2d844337a632df5121008bb27703c4f144
Brandon Garcia <grandonleebarcia@gmail.com>
2021-10-11 21:29:45 -0700

    removed all components except ones I am revamping the design for

--- src/App.js
+++ src/App.js
@@ -10,11 +10,9 @@ const App = () => {
   return (
     <div className="App">
       <NavBar />
-      <Container fluid={"xl"}>
+      <Container fluid className="p-0">
         <LandingPage />
         <AboutMe />
-        <Projects />
-        <Contact />
       </Container>
     </div>
   );
